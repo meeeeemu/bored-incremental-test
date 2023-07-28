ad96eee412c42dd082ddd94495a6297e6864deea
meeeeemu <memedream636@gmail.com>
2023-07-28 01:11:37 +0100

    rebirths maybe

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ if(parseddata){
         intervalCost: new Decimal(parseddata.intervalCost),
         intervalTime: parseddata.intervalTime,
         decimalPlaceCount: parseddata.decimalPlaceCount,
-        rebirthMultiplier: new Decimal(parseddata.rebirthMultiplier)
+        rebirthMultiplier: new Decimal(parseddata.rebirthMultiplier),
+        rebirthCounts: new Decimal(parseddata.rebirthCounts)
     }
 } else {
     var data = {
@@ -24,7 +25,8 @@ if(parseddata){
         intervalCost: new Decimal(0.001),
         intervalTime: 500,
         decimalPlaceCount: 6,
-        rebirthMultiplier: new Decimal(1)
+        rebirthMultiplier: new Decimal(1),
+        rebirthCounts: new Decimal(0)
     }
 }
 
@@ -73,23 +75,45 @@ document.getElementById("saveButton").addEventListener('click', function () {
 });
 
 function mainIncrement(){
-    data.pointValue = (data.pointValue.add(data.incrementValue));
+    data.pointValue = (data.pointValue.add(data.incrementValue.multiply(data.rebirthMultiplier)));
     document.getElementById("pointValue").innerHTML = data.pointValue.toPrecision(data.decimalPlaceCount);
     if(document.getElementById("pointCostInterval")){
-        document.getElementById("pointCostInterval").innerHTML = ((data.pointValue.multiply(16))/50).toPrecision(data.decimalPlaceCount)
+        document.getElementById("pointCostInterval").innerHTML = ((data.pointValue.multiply(16))/50).toPrecision(data.decimalPlaceCount);
     }
     if(data.intervalTime==1){
         document.getElementById("upgradeInterval").innerText = "he's going fast (max)";
         document.getElementById("upgradeInterval").style = "cursor: not-allowed; pointer-events: none;";
     }
+    document.getElementById('rebirthPotential').innerHTML = data.rebirthMultiplier.toPrecision(data.decimalPlaceCount)
 }
+
+document.getElementById('rebirthButton').addEventListener('click', function(){
+    if(data.pointValue>=1e6){
+        data.rebirthMultiplier = data.rebirthMultiplier.add(0.1);
+        document.getElementById('rebirthPotential').innerHTML = data.rebirthMultiplier.toPrecision(data.decimalPlaceCount)
+        data.pointValue = new Decimal(0);
+        data.incrementValue = new Decimal(0.001);
+        data.upgradeCost = new Decimal(0.01);
+        data.intervalCost = new Decimal(0.001);
+        data.intervalTime = 500;
+        data.decimalPlaceCount = 6;
+        data.rebirthCounts = data.rebirthCounts.add(1);
+        document.getElementById('currentPointIncrement').innerHTML = data.incrementValue.multiply(data.rebirthMultiplier).toPrecision(data.decimalPlaceCount);
+        document.getElementById('upgradeCost').innerHTML = data.upgradeCost.toPrecision(data.decimalPlaceCount);
+        document.getElementById('currentPointInterval').innerHTML = data.intervalTime;
+        document.getElementById("upgradeInterval").innerHTML = "convert <div id='pointCostInterval'>0</div> to lower interval value";
+        document.getElementById("upgradeInterval").style = "";
+    }
+})
+
+
 
 document.getElementById("upgradeIncrement").addEventListener('click', function(){
     console.log(data.pointValue.cmp(data.upgradeCost));
     if(data.pointValue.cmp(data.upgradeCost)==1){
         data.pointValue = data.pointValue.subtract(data.upgradeCost);
-        data.upgradeCost = data.upgradeCost.multiply(3.141);
-        data.incrementValue = data.incrementValue.multiply(2.239482345);
+        data.upgradeCost = (data.upgradeCost.multiply(3.141)).div(data.rebirthMultiplier);
+        data.incrementValue = data.incrementValue.multiply(2.482345);
         document.getElementById("upgradeCost").innerHTML = data.upgradeCost.toPrecision(data.decimalPlaceCount);
         document.getElementById("currentPointIncrement").innerHTML = data.incrementValue.toPrecision(data.decimalPlaceCount);
     }
